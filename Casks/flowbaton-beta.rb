dadfb96d8b56dd62919306d8eb820fe070b73beb
larchwave <314023723+larchwave@users.noreply.github.com>
2026-09-04 19:27:40 -0400

    docs: describe the published beta cask

--- Casks/flowbaton-beta.rb
+++ Casks/flowbaton-beta.rb
@@ -14,6 +14,6 @@ cask "flowbaton-beta" do
   end
 
   name "FlowBaton"
-  desc "Pre-alpha mobile UI automation toolkit"
+  desc "Mobile UI automation toolkit"
   homepage "https://github.com/larchwave/flowbaton"
 end
